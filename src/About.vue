82302b14b3fe4c4fc0a31ffe588dce1cefed3508
Eva Garcia Pinos <eva.gapi@gmail.com>
2022-08-27 14:26:02 +0200

    feat: add component to App

---
+++ src/About.vue
@@ -0,0 +1,25 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    <h1>Hello World!</h1>
+    <p>{{ aboutMe }}</p>
+  </div>
+</template>
+<script>
+const aboutMe = `
+I ❤️ cheese, especially queso blue castello. 
+Parmesan cheese slices caerphilly edam feta cheese 
+and biscuits mascarpone cauliflower cheese.
+Mozzarella cauliflower cheese fromage cow 
+pecorino danish fontina pecorino stinking bishop. 
+Cream cheese fondue manchego parmesan smelly cheese.
+`;
+
+export default {
+  data() {
+    return {
+      aboutMe,
+    };
+  },
+};
+</script>
